9144e3c4550264359f4e1b0d07fb25adcb396847
jvv00 <jvv0006@gmail.com>
2023-12-18 14:33:40 +0900

    Add media query tablet , mobile

--- 05-exam/js/comm.js
+++ 05-exam/js/comm.js
@@ -2,12 +2,12 @@ const swiper = new Swiper(".swiper", {
   // Optional parameters
   // direction: "vertical",
   loop: true,
-  // slidesPerView: 3,
-  spaceBetween: 30,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+  slidesPerView: 1,
+  // spaceBetween: 30,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
 
   // If we need pagination
   pagination: {
