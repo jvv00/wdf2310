bd7bece1b30f0e345b9924c1e5d4afc6b3a07bdb
jvv00 <jvv0006@gmail.com>
2024-01-17 14:34:02 +0900

    Add js연산자.js : 병합 연산자, Create js구문.js

--- 07-JS/js/js연산자.js
+++ 07-JS/js/js연산자.js
@@ -91,4 +91,76 @@ console.log(true || true);
 console.log(true || false);
 console.log(false || false);
 
+// 연산자를 기준으로 가장 먼저 만나는 true 값을 반환한다.
+console.log(false || true);
+console.log(1 || 0);
+console.log(false || 0 || {});
+console.log("" || 0 || NaN);
+// 모두가 false일 경우 마지막 값을 반환한다. 
+
 // 병합 연산자
+// || 연산자와 비슷하다.
+// null 또는 undefined는 제외하고 반환한다.
+
+console.log(0 || 5);
+console.log(0 ?? 5);
+console.log(null || undefined);
+
+// 삼항 연산자
+// 조건 ? 참일 때 반환 : 거짓일 때 반환
+console.log(1 > 2 ? "참" : "거짓");
+
+function isCat(name){
+    return (
+        name === "고양이" ? "고양이" : "고양이 아님"
+    );
+}
+console.log(isCat("고양이"));
+console.log(isCat("강아지"));
+
+// 전개 연산자
+// ...을 사용하여 배열이나 객체를 펼칠 수 있다.
+
+const num1 = [1, 2, 3];
+const num2 = [4, 5, 6];
+
+console.log(num1.concat(num2));
+console.log([...num1, ...num2]);
+
+// 객체의 전개
+const dog = {
+    name: "멍멍이",
+    phone: "010-0000-0000",
+
+};
+const dogInfo = {
+    ...dog,
+    age: 2,
+};
+
+console.log(dogInfo);
+
+// 함수의 전개
+// ...을 사용하여 파라미터(인수)를 전개할 수 있다.
+
+// sum 함수 선언
+
+function sum(a, b, c){
+    console.log(a + b + c);
+}
+
+// sum 함수 호출
+sum(1, 3, 5);
+
+// 배열을 파라미터(인수)로 전달하려면?
+const num3 = [5, 6, 7];
+const num4 = [8, 9, 10];
+
+sum(num3[0], num3[1], num3[2]);
+sum(num4[0], num4[1], num4[2]);
+
+// 전개 연산자를 사용해 인수를 전달
+sum(...num3);
+sum(...num4);
+console.log()
+
