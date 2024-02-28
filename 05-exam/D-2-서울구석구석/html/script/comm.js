7d8ae06bc729482c201aaac1996fb6f3b76ab375
jvv00 <jvv0006@gmail.com>
2024-02-28 14:51:00 +0900

    webdesign-exam d-2/slide 배치

---
+++ 05-exam/D-2-서울구석구석/html/script/comm.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+//     $(".menu").mouseover(function(){
+//         $(this).find(".depth2").stop().slideDown();
+//     });
+//     $(".menu").mouseout(function(){
+//         $(this).find(".depth2").stop().slideUp();
+//     })
+// })
