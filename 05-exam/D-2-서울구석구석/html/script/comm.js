ca16d350baf4b3302eb0598c2f48afe886ba6d75
jvv00 <jvv0006@gmail.com>
2024-03-04 16:06:19 +0900

    웹디기능사 철길마을

--- 05-exam/D-2-서울구석구석/html/script/comm.js
+++ 05-exam/D-2-서울구석구석/html/script/comm.js
@@ -6,3 +6,44 @@
 //         $(this).find(".depth2").stop().slideUp();
 //     })
 // })
+
+$(function () {
+    // 현재 보이는 슬라이드의 인덱스
+    let now = 0;
+    // 슬라이드 개수
+    const slideCount = $('.slide li').length
+    const slideList = $('.slide')
+
+    // // 슬라이드 이동 함수
+    // function moveSlide() {
+    //     slideList.css('left', 100 * -now + '%')
+    // }
+
+    setInterval(function (){
+        // now값을 1씩 증가시키고
+        // slidecount(3)으로 나눈 나머지 값을 now에 대입
+        // (0 + 1) % 3 = 1(나머지)
+        now = (now + 1) % slideCount
+        slideList.css('left', 100 * -now + '%')
+    }, 3000)
+})
+
+// tabs
+$(function () {
+    $(".tab-tit").on('click', function(event){
+
+        event.preventDefault()
+
+        $('.tab').removeClass('active')
+        $(this).parents('.tab').addClass('active')
+    })
+})
+
+$(function (){
+    $(".bbs li:first-child").on("click", function(){
+        $("#modal").css("display", "flex");
+    })
+    $("#modal button").on("click", function(){
+        $("#modal").css("display", "none");
+    })
+})
